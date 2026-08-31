c009f3f08995fd097160e5010bc3a3bfa2e0d7d8
prashdraksh <prashdraksh@gmail.com>
2026-08-31 16:20:12 +0530

    model trained with good accurcy

--- src/components/data_ingetion.py
+++ src/components/data_ingetion.py
@@ -4,6 +4,7 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.modeltrainer import ModelTrainerConfig,ModelTrainer
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -49,4 +50,7 @@ if __name__=="__main__":
     train_path,test_path=obj.initiate_data_ingestion()
 
     data_transformation_obj=DataTransformation()
-    data_transformation_obj.initiate_data_transformation(train_path,test_path)
+    train_arr,test_arr,_=data_transformation_obj.initiate_data_transformation(train_path,test_path)
+
+    model_training=ModelTrainer()
+    print(model_training.initiate_model_trainer(train_arr,test_arr))
